0e20daa73623fa9c986307778002e89f0061d69a
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-03 13:00:24 +0200

    Esimene commit, 03.01.2018, index-php

---
+++ index.php
@@ -0,0 +1,12 @@
+  <?php
+
+$greeting = "Hello";
+$nr = 1;
+$nr = 10.0121;
+$bool = False;
+$massiiv = array('Nimi',1947,10.81,'Perenimi');
+
+echo "$greeting world!<br>";
+echo '$greeting world!<br>';
+echo $massiiv[0];
+ ?>
